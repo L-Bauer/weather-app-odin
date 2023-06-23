8c76de9d3e8ceeff4aa15208f53441222555c308
Lucas D. Bauer <lucassoccer95@gmail.com>
2023-06-23 14:33:22 -0500

    Setup getCurrentWeather function with error checks

--- src/app/api.js
+++ src/app/api.js
@@ -1,15 +1,30 @@
 const apiKey = "12708416b49647a2b90145252232306";
 
-async function getCityInfo(City) {
-  const response = await fetch(
-    `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${City}`,
-    {
-      mode: "cors",
-      method: "GET",
-    },
-  )
-    .catch((err) => err.json());
-  return response.json();
+async function getCurrentWeather(Place) {
+  // Get the current weather for the location
+  let response;
+  // fetch weather data from weather api
+  try {
+    response = await fetch(
+      `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${Place}`,
+      {
+        mode: "cors",
+        method: "GET",
+      },
+    );
+  } catch (error) {
+    // Error if the fetch operation is not completed
+    console.log("There has been a problem with your fetch operation:", error);
+  }
+
+  let placeData;
+  // If the response from the fetch has an error, log error status
+  if (response?.ok) {
+    placeData = await response.json();
+  } else {
+    console.log(`HTTP Response Code: ${response?.status}`);
+  }
+  return placeData;
 }
 
-export { getCityInfo };
+export { getCurrentWeather };
