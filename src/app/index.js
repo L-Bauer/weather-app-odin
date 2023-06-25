b071b2ac7e541f5b3e5d9de337d3a1775f040a03
lucas <lucassoccer95@gmai.com>
2023-06-24 19:38:10 -0500

    Remove error check

--- src/app/index.js
+++ src/app/index.js
@@ -3,8 +3,5 @@ import { getCurrentWeather, getForecastWeather } from "./api";
 const info = await getCurrentWeather("London");
 console.log(info);
 
-const second = await getCurrentWeather("L12Lds");
-console.log(second);
-
 const forecast = await getForecastWeather("Chicago");
 console.log(forecast);
