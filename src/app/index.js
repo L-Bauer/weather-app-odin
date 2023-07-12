667e026dcd165dc854231e331c9b5a69cd72eb1d
Lucas D. Bauer <lucassoccer95@gmail.com>
2023-07-12 10:49:18 -0500

    Create function to get data from form

--- src/app/index.js
+++ src/app/index.js
@@ -1,17 +1,16 @@
 import { getForecastWeather } from "./api";
 
-const search = document.querySelector(".search-box-input");
-const input = search.value;
+const submitSearch = document.querySelector("#search-form");
 
-const forecast = await getForecastWeather("Fond du Lac");
-console.log(forecast);
+// const forecast = await getForecastWeather("Fond du Lac");
+// console.log(forecast);
 
-let data;
-
-search.addEventListener("keydown", async (e) => {
-  if (e.key === "Enter") {
-    data = await getForecastWeather(input);
-  }
+submitSearch.addEventListener("submit", async (e) => {
+  // const data = await getForecastWeather(input);
+  e.preventDefault();
+  const form = new FormData(submitSearch);
+  const location = form.get("location");
+  console.log(location);
+  const forecast = await getForecastWeather(location);
+  console.log(forecast);
 });
-
-console.log(data);
