f0a91ede3b0a16e364f899ef64f31e32323247cb
Lucas Bauer <lucas.d.bauer95@gmail.com>
2023-07-12 06:34:36 -0500

    Add search bar for location

--- src/app/index.js
+++ src/app/index.js
@@ -1,4 +1,17 @@
 import { getForecastWeather } from "./api";
 
+const search = document.querySelector(".search-box-input");
+const input = search.value;
+
 const forecast = await getForecastWeather("Fond du Lac");
 console.log(forecast);
+
+let data;
+
+search.addEventListener("keydown", async (e) => {
+  if (e.key === "Enter") {
+    data = await getForecastWeather(input);
+  }
+});
+
+console.log(data);
