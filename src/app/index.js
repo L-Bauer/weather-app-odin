c7de4b7985268e106ded544eaabdae6c9395fe00
lucas <lucassoccer95@gmai.com>
2023-07-15 18:16:31 -0500

    Bug with npm

--- src/app/index.js
+++ src/app/index.js
@@ -1,32 +1,34 @@
-import { version } from "html-webpack-plugin";
-import { getForecastWeather } from "./api";
+import { version } from "html-webpack-plugin"
+import { getForecastWeather } from "./api"
 
-const submitSearch = document.querySelector("#search-form");
-const main = document.querySelector("main");
+const submitSearch = document.querySelector("#search-form")
+const main = document.querySelector("main")
 
-let forecast;
+let forecast
 
-function views(data) {
-  console.log(data.current.temp_f);
-  console.log(data.current.condition.text);
-  console.log(data.current.feelslike_f);
+function views (data) {
+  console.log(data.current.temp_f)
+  console.log(data.current.condition.text)
+  console.log(data.current.feelslike_f)
 }
 
 const viewFactory = (weatherData) => {
   const mainView = () => {
-    const location = weatherData.location.name;
-    const region = weatherData.location;
-    const p = document.createElement("p");
-    main.appendChild(p.textContent = location);
-  };
-  return mainView;
-};
+    const location = weatherData.location.name
+    const region = weatherData.location
+    const p = document.createElement("p")
+    main.appendChild(p.textContent = location)
+  }
+  return mainView
+}
 
 submitSearch.addEventListener("submit", async (e) => {
-  e.preventDefault();
-  const form = new FormData(submitSearch);
-  const city = form.get("location");
-  forecast = await getForecastWeather(city);
-  console.log(forecast);
-  views(forecast);
-});
+  e.preventDefault()
+  const form = new FormData(submitSearch)
+  const city = form.get("location")
+  forecast = await getForecastWeather(city)
+  const views = viewFactory(forecast)
+  views.mainView()
+  console.log(forecast)
+  views(forecast)
+})
