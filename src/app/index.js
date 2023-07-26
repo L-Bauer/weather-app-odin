53be508ab282fa2b33c3b3e944eb2d475c019013
Lucas D. Bauer <lucassoccer95@gmail.com>
2023-07-26 09:18:09 -0500

    Add in region and current temp to webpage

--- src/app/index.js
+++ src/app/index.js
@@ -8,13 +8,15 @@ let forecast
 function mainView (weatherData) {
   const location = weatherData.location.name
   const region = weatherData.location.region
+  const currentTemp = weatherData.current.temp_f
 
-  const regionHTML = document.createElement('p')
-  const locationHTML = document.createElement('p')
+  const regionHTML = document.querySelector('.region')
+  const locationHTML = document.querySelector('.location')
+  const currentTempHtml = document.querySelector('.current')
+  
   regionHTML.innerText = region
   locationHTML.innerText = location
-  main.appendChild(locationHTML)
-  main.appendChild(regionHTML)
+  currentTempHtml.innerText = "Current Temp: " + currentTemp
 }
 
 submitSearch.addEventListener('submit', async (e) => {
